4c450eb76e4f2f84912bae5c0083dfb94cea6426
RostYakuts <rostwirt@gmail.com>
2026-02-18 21:22:55 +0200

    Remove vaste

--- wp-content/themes/dermideal/assets/src/js/pages/home/home.js
+++ wp-content/themes/dermideal/assets/src/js/pages/home/home.js
@@ -1,8 +1,6 @@
 import '@/scss/pages/home/home.scss';
 
-import {casesSwiper} from "@/js/pages/home/components/casesSwiper";
 import {testimonialsSwiper} from "@/js/pages/home/components/testimonialsSwiper";
-import {intTel} from "@/js/form/intTel";
 import {advantagesSlider} from "@/js/pages/home/components/advantagesSlider";
 import {bgSwiper} from "@/js/pages/home/components/bgSwiper";
 import {bestsellersSwiper} from "@/js/pages/home/components/bestsellersSwiper";
@@ -10,6 +8,4 @@ import {bestsellersSwiper} from "@/js/pages/home/components/bestsellersSwiper";
 bgSwiper();
 bestsellersSwiper();
 advantagesSlider();
-casesSwiper();
 testimonialsSwiper();
-intTel();
